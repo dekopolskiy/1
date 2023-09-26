a12b0a3119e711b0ae472c5d5608e99e18f2b465
dmitri <dmitridecopolski@yandex.ru>
2023-09-26 15:28:45 +0300

    first commit123

---
+++ menu.js
@@ -0,0 +1,19 @@
+function hello() {
+  setTimeout(function () {
+    console.log("hello world set");
+  }, 2000);
+  console.log("hello world");
+}
+
+{
+  {
+    {
+      {
+        {
+          {
+          }
+        }
+      }
+    }
+  }
+}
